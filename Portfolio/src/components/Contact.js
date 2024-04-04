ef54bc0d3c4a0e03d799a3ca7e708ab273882fcb
rikhisingh <rikhisinghnov@gmail.com>
2024-04-04 12:12:13 -0400

    define contact page sizing

--- Portfolio/src/components/Contact.js
+++ Portfolio/src/components/Contact.js
@@ -5,7 +5,20 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+  return (
+    <div className='py-16 lg:section' id='contact'>
+      <div className="container mx-auto">
+        <div>
+          {/* text */}
+          <div>text</div>
+          {/* fomr */}
+          <form action="">
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
