a524c529143a7ba2d23d1085160f1f684b285594
rikhisingh <rikhisinghnov@gmail.com>
2024-04-04 12:27:23 -0400

    contact message completed

--- Portfolio/src/components/Contact.js
+++ Portfolio/src/components/Contact.js
@@ -8,13 +8,20 @@ const Contact = () => {
   return (
     <div className='py-16 lg:section' id='contact'>
       <div className="container mx-auto">
-        <div>
+        <div className='flex flex-col lg:flex-row'>
           {/* text */}
-          <div>text</div>
+          <motion.div variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 flex justify-start items-center'>
+            <div>
+              <h4 className='text-xl text-accent uppercase font-medium mb-2 tracking-wide'>
+                Get in Touch
+              </h4>
+              <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>
+                LEt's work <br />
+                together !
+              </h2>
+            </div>
+          </motion.div>
           {/* fomr */}
-          <form action="">
-
-          </form>
         </div>
       </div>
     </div>
