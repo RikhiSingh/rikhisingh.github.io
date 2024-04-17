9af424e0c2683701597ece2ec909d0cf48600bb1
rikhisingh <rikhisinghnov@gmail.com>
2024-04-17 17:47:37 -0400

    portfolio

---
+++ Portfolio/src/components/Banner.js
@@ -0,0 +1,85 @@
+import React from 'react';
+// images
+import Image from '../assets/avatar.svg';
+// icons
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+// type animation
+import { TypeAnimation } from 'react-type-animation';
+// motion
+import { motion } from 'framer-motion';
+// variants
+import { fadeIn } from '../variants';
+// github link
+const githubLink = "https://github.com/RikhiSingh/";
+// linkedIn link
+const linkedInLink = "https://www.linkedin.com/in/rikhi-singh/";
+// instagram link
+const InstagramLink = "https://www.instagram.com/rikhi_singh/";
+
+
+
+
+const Banner = () => {
+  const downloadResume = () => {
+    const link = document.createElement('a');
+    link.href = process.env.PUBLIC_URL + '/resume.pdf';
+    link.download = 'RikhiSingh - Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
+  return (
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+      <div className="container mx-auto mt-[-60px] md:mt-[-100px] lg:mt-0">
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          {/* text */}
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1 variants={fadeIn('up', 0.22)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'><span className='text-white text-[40px] font-bold leading-[0.8] lg:text-[89px]'>I</span> <span className='text-white text-[35px] font-bold leading-[0.8] lg:text-[70px]'>am</span> RIKHI </motion.h1>
+            <motion.div variants={fadeIn('up', 0.46)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+              
+              < TypeAnimation sequence={[
+                'Full Stack Developer',
+                2000,
+                'CyberSecurity Analyst',
+                2000,
+                'Software Developer',
+                2000,
+                'Penetration Testing',
+                2000,
+              ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p variants={fadeIn('up', 0.7)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-8 max-w-lg mx-auto lg:mx-0'>Certified from University of Michigan, Google, University of Helsinki, Microsoft and Member of CIPS Ontario</motion.p>
+            <motion.div variants={fadeIn('up', 0.94)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+              <button className='btn btn-lg' onClick={downloadResume}>My Portfolio</button>
+              <a href={linkedInLink} target='_blank' className='text-gradient btn-link'>Contact Me!</a>
+            </motion.div>
+            {/* socials */}
+            <motion.div variants={fadeIn('up', 1.18)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+              <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+              </a>
+              <a href={linkedInLink} target="_blank" rel="noopener noreferrer">
+                <FaLinkedin />
+              </a>
+              <a href={InstagramLink} target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+              </a>
+            </motion.div>
+          </div>
+          {/* image */}
+          <motion.div variants={fadeIn('down', 0.46)} initial='hidden' whileInView={'show'} className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
+            <img className='w-[482px] h-[512px]' src={Image} alt="" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
---
+++ Portfolio/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// images
+import Logo from '../assets/logo.png'
+
+const Header = () => {
+
+  const openLinkedInProfile = () => {
+    window.open('https://www.linkedin.com/in/rikhi-singh/', '_blank');
+  };
+
+  return <header className='py-7' id='header'>
+    <div className="container mx-auto">
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href="#header">
+          <img className='w-[60px]' src={Logo} alt="" />
+        </a>
+        {/* button */}
+        <button className='btn btn-sm' onClick={openLinkedInProfile}>
+          Work with ME!
+        </button>
+      </div>
+    </div>
+  </header>;
+};
+
+export default Header;
