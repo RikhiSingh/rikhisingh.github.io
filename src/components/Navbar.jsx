b3e76bd84ac45ddc6c3f7a7af555289351934937
rikhisingh <rikhisinghnov@gmail.com>
2024-05-01 11:12:51 -0400

    skeleton finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+// to navigate to other parts of the page
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+
+    </nav>
   )
 }
 
