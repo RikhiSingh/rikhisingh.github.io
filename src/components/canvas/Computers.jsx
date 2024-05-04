ee6885a3bcc4ec102b8b516d0864d62f4a725a22
rikhisingh <rikhisinghnov@gmail.com>
2024-05-04 01:16:35 -0400

    lightning created

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,9 +5,16 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader'; 
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}  groundColor="black" />
+      <pintLight intensity={1} />
+      <primitive 
+        object={computer.scene}
+      />
+    </mesh>
   )
 }
 
