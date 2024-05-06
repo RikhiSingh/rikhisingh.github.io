33fad2c44ca4ae26afc60e8399fbe4101e69f9cc
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 18:14:55 -0400

    layout fixed for navbar and children

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-red-500">
-          <Navbar />
-          {children}
+        <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+          <div className="h-24">
+            <Navbar />
+          </div>
+          <div className="h-[calc(100vh - 6rem)]">
+            {children}
+          </div>
         </div>
       </body>
     </html>
