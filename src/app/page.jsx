2f00ee20d9e5faebca3addca12a8fbdb07936168
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 19:47:48 -0400

    Hero Image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 const Homepage = () => {
-  return <div className="flex flex-col"> 
+  return <div className="h-full flex flex-col"> 
     {/* Image container */}
-    <div className="h-1/2">
-      <Image src={}/>
+    <div className="h-1/2 relative bg">
+      <Image src='/hero.webp' alt="hero Image" fill className="object-contain"/>
     </div>
     {/* Text container */}
     <div className="h-1/2"></div>
