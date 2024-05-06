c92dcf5aa7bcb635df77ede1d2a529481bc823f7
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 19:28:55 -0400

    container made

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return <div className="flex flex-col"> 
+    {/* Image container */}
+    <div className="h-1/2">
+      <Image src={}/>
+    </div>
+    {/* Text container */}
+    <div className="h-1/2"></div>
+  </div>;
 };
 
 export default Homepage;
