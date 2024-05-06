95b3b8b55b847f15f597754a66bc24f7acf1c9e8
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 18:06:17 -0400

    basic css to layout div

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        </body>
+        <div className="w-screen h-screen bg-red-500">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
