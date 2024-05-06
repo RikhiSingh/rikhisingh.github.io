213e2f01d6dfa6f14f3d214cadccf50be0fec1dd
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 18:56:19 -0400

    on click styling to navlinks

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -6,10 +6,9 @@ import { usePathname } from "next/navigation";
 const NavLink = ({link}) => {
 
     const pathName = usePathname();
-    console.log(pathName);
 
     return ( 
-        <Link href={link.url} className="">{link.title}</Link>
+        <Link href={link.url} className={`rounded p-1 ${pathName === link.url && 'bg-black text-white'}`}>{link.title}</Link>
      );
 }
  
