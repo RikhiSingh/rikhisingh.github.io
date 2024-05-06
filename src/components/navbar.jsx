be37e55c3d0dffa84c2491ff966d92be663082d3
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 18:21:44 -0400

    logo finished

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return ( 
-        <div className="">
-            Navbar
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            {/* Logo */}
+            <div className="">
+                <Link href='/' className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
+                    <span className="text-white mr-1">Rikhi</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Singh</span>
+                </Link>
+            </div>
+            {/* menu */}
+            <div className="">2</div>
         </div>
      );
 }
