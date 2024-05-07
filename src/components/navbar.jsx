66bcde449bb5dcc92fffdfd5a61c86d401ba0588
rikhisingh <rikhisinghnov@gmail.com>
2024-05-07 18:25:36 -0400

    navbar hamburger menu finsihed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import NavLink from "./navLink";
+import { motion } from "framer-motion";
 
 const links = [
     { url: '/', title: "Home" },
@@ -14,12 +15,67 @@ const links = [
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+
+    const topVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: 45,
+            backgroundColor: "rgb(255,255,255)"
+        },
+    };
+
+    const centerVariants = {
+        closed: {
+            opacity: 1,
+        },
+        opened: {
+            opacity: 0,
+        },
+    };
+
+    const bottomVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)"
+        },
+    };
+
+    const listVariants = {
+        closed: {
+            x: "100vw"
+        },
+        opened: {
+            x: 0,
+            transition:{
+                when:'beforeChildren',
+                staggerChildren:0.221,
+            }
+        },
+    };
+
+    const listItemVariants={
+        closed:{
+            x:-10,
+            opacity:0
+        },
+        opened:{
+            x: 0,
+            opacity:1,
+        },
+    };
+
+
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
             {/* LINKS */}
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map((link) => (
-                    <NavLink link={link} key={link.title}/>
+                    <NavLink link={link} key={link.title} />
                 ))}
             </div>
             {/* LOGO */}
@@ -53,18 +109,42 @@ const Navbar = () => {
                     className="w-10 h-8 flex flex-col justify-between z-50 relative"
                     onClick={(() => setOpen((prev) => !prev))}
                 >
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <motion.div
+                        variants={topVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"
+                    ></motion.div>
+                    <motion.div
+                        variants={centerVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"
+                    ></motion.div>
+                    <motion.div
+                        variants={bottomVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"
+                    ></motion.div>
                 </button>
                 {/* Menu List */}
                 {open && (
 
-                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                    <motion.div
+                        variants={listVariants}
+                        initial="closed"
+                        animate="opened"
+                        className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+                    >
                         {links.map(link => (
-                            <Link href={link.url} key={link.title}>{link.title}</Link>
+                            <motion.div 
+                            variants={listItemVariants}
+                                key={link.title}
+                            >
+                                <Link href={link.url}>
+                                    {link.title}
+                                </Link>
+                            </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
                 )}
             </div>
         </div>
