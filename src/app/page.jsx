6429040b5efb2e9e5723a6ec540cac28fd459a51
rikhisingh <rikhisinghnov@gmail.com>
2024-05-06 20:10:03 -0400

    home finished

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
 const Homepage = () => {
-  return <div className="h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+  return <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
     {/* Image container */}
-    <div className="h-1/2 relative bg">
+    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
       <Image src='/hero.webp' alt="hero Image" fill className="object-contain" />
     </div>
     {/* Text container */}
-    <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
       {/* title */}
       <h1 className="text-4xl md:text-6xl font-bold">Hi, I am Rikhi</h1>
       {/* description */}
-      <p className="md:text-xl flex flex-col justify-center items-center"><span className="font-semibold text-2xl md:text-4xl">Web and Software Developer</span> <br /> Certified from University of Michigan, Google, University of Helsinki, Microsoft and Member of CIPS Ontario</p>
+      <p className="md:text-xl">Web and Software Developer and expertise in Cybersecurity with Certification from University of Michigan, Google, University of Helsinki, Microsoft and Member of CIPS Ontario</p>
       {/* Buttons */}
-      <div className="flex gap-4">
+      <div className="flex gap-4 lg:w-full">
         <div className="p-4 rounded-lg ring-1 ring-black bg-black text-white">See my Resume</div>
         <div className="p-4 rounded-lg ring-1 ring-black bg-black text-white">See my work</div>
       </div>
