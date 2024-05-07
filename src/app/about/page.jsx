38803bf5a7e683a096c4127b11790bb355a2196f
rikhisingh <rikhisinghnov@gmail.com>
2024-05-07 18:52:08 -0400

    motion added to each when its data is rendered after animation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 const AboutPage = () => {
-    return ( 
-        <div className="">
-            AboutPage
-        </div>
-     );
+    return (
+        <motion.div
+            className="h-full"
+            initial={{ y: "-200vh" }}
+            animate={{ y: "0%" }}
+            transition={{ duration: 1 }}
+        >
+            About
+        </motion.div>
+    );
 }
- 
+
 export default AboutPage;
