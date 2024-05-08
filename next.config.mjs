fec69af2c65a53df425fb3b6058311c0b1892f39
rikhisingh <rikhisinghnov@gmail.com>
2024-05-08 09:34:02 -0400

    portfolio finished

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:"https",
+                hostname:'images.pexels.com',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
+
+
+
+// Default:
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
