6ca4b356caddab7394d7f280a894b5ea501f679d
rikhisingh <rikhisinghnov@gmail.com>
2024-05-08 18:07:17 -0400

    added favicon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Head from "next/head";
 
 const Homepage = () => {
 
@@ -32,6 +33,9 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" />
+      </Head>
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* Image container */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
