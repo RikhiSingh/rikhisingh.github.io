bb579ead2ffa6b54825c6f74bd1542893019200a
rikhisingh <rikhisinghnov@gmail.com>
2024-05-08 16:46:46 -0400

    loader and server config before export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images:{
         remotePatterns:[
             {
+                loader: "custom",
+                loaderFile: "",
                 protocol:"https",
                 hostname:'images.pexels.com',
             },
