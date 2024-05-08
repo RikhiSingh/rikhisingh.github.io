0b36be354ebd1de8670812ac72ac5b8034e7de65
rikhisingh <rikhisinghnov@gmail.com>
2024-05-08 11:25:02 -0400

    added scroll svg

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -53,8 +53,43 @@ const PortfolioPage = () => {
             transition={{ duration: 1 }}
         >
             <div className="h-[600vh] relative" ref={ref}>
-                <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
-                    My Works
+                <div className="w-screen h-[calc(100vh-6rem)] flex flex-col items-center justify-center text-8xl text-center">
+                    <div>
+                        My Works
+                    </div>
+                    <div className="flex justify-center items-center text-sm ">
+                        <motion.svg
+                            className="mt-[calc(100%-0px)] bottom-16"
+                            initial={{ opacity: 0.2, y: 0 }}
+                            animate={{ opacity: 1, y: "10px" }}
+                            transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={50}
+                            height={50}
+                        >
+                            <path
+                                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                                stroke="#000000"
+                                strokeWidth="1"
+                            ></path>
+                            <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+                            <path
+                                d="M15 11L12 14L9 11"
+                                stroke="#000000"
+                                strokeWidth="1"
+                            ></path>
+                        </motion.svg>
+                        <motion.div
+                            className="mt-[calc(100%-0px)] bottom-16"
+                            initial={{ opacity: 0.2, y: -15 }}
+                            animate={{ opacity: 1, y: "25px" }}
+                            transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+                        >
+                            Scroll Down
+                        </motion.div>
+                    </div>
                 </div>
                 <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
                     <motion.div style={{ x }} className="flex">
@@ -100,7 +135,7 @@ const PortfolioPage = () => {
                         </defs>
                         <text fill="#000">
                             <textPath xlinkHref="#circlePath" className="text-xl">
-                                Front-end Developer and UI Designer
+                                Software and Web Developer
                             </textPath>
                         </text>
                     </motion.svg>
