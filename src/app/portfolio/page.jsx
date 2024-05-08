1bf85131c328cdb23a88e43e7000340423daedf4
rikhisingh <rikhisinghnov@gmail.com>
2024-05-08 11:07:35 -0400

    portfolio content

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -24,18 +24,18 @@ const items = [
     {
         id: 3,
         color: "from-violet-300 to-purple-300",
-        title: "",
-        desc: "",
+        title: "Portfolio Website",
+        desc: "Developed Portfolio Website using Next.JS, Tailwind CSS, MailJS, Motion-Framer and latest web technologies!",
         img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-        link: "https://",
+        link: "https://github.com/RikhiSingh",
     },
     {
         id: 4,
         color: "from-purple-300 to-red-300",
-        title: "",
-        desc: "",
+        title: "Upcoming..",
+        desc: "In Development (Private Repo)",
         img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        link: "https://",
+        link: "https://github.com/RikhiSingh",
     },
 ];
 
