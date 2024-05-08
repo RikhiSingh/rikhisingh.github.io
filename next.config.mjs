e9e2676107c576b37b3bda6f5403ed1ea35d8331
rikhisingh <rikhisinghnov@gmail.com>
2024-05-08 18:42:36 -0400

    updated portfolio and added image and removed pexels outgoing from config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images:{
-        // loader: "custom",
-        // loaderFile: "./loader.js",
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:'images.pexels.com',
-            },
-        ],
-    },
 };
 
 export default nextConfig;
