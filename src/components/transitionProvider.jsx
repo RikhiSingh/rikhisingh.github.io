2c05087b4284e99427ffc8e19f1572c55b2bfe96
rikhisingh <rikhisinghnov@gmail.com>
2024-05-10 22:53:35 -0400

    fixed styling build issue

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -11,9 +11,13 @@ const TransitionProvider = ({ children }) => {
         <AnimatePresence mode="wait">
             <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
                 {/* <!-- Google Tag Manager (noscript) --> */}
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDG9CTG7"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                <noscript>
+                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDG9CTG7"
+                        height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
+                    </iframe>
+                </noscript>
                 {/* <!-- End Google Tag Manager (noscript) --> */}
+
                 <motion.div
                     className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
                     animate={{ height: "0vh" }}
@@ -33,7 +37,7 @@ const TransitionProvider = ({ children }) => {
                     className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30"
                     initial={{ height: "140vh" }}
                     animate={{ height: "0vh", transition: { delay: 0.5 } }}
-                /> 
+                />
                 <div className="h-24">
                     <Navbar />
                 </div>
