899c436d1fd9aeec5e3461295767bcd2e04ad4b7
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 12:52:06 -0400

    removed div added link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,14 +6,13 @@ import Head from "next/head";
 import Link from "next/link";
 
 const Homepage = () => {
-
   const downloadResume = () => {
     // Create an anchor element
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     // Set the href attribute to the path of your resume PDF
-    link.href = './RikhiSingh - Resume.pdf';
+    link.href = "./RikhiSingh - Resume.pdf";
     // Set the download attribute to force download the file
-    link.download = 'RikhiSingh - Resume.pdf';
+    link.download = "RikhiSingh - Resume.pdf";
     // Append the anchor element to the body
     document.body.appendChild(link);
     // Trigger a click event on the anchor element
@@ -50,18 +49,37 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* Image container */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src='./hero.webp' alt="hero Image" fill className="object-contain" />
+          <Image
+            src="./hero.webp"
+            alt="hero Image"
+            fill
+            className="object-contain"
+          />
         </div>
         {/* Text container */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* title */}
           <h1 className="text-4xl md:text-6xl font-bold">Hi, I am Rikhi</h1>
           {/* description */}
-          <p className="md:text-xl">Web and Software Developer with expertise in Cybersecurity with Certification from University of Michigan, Google, University of Helsinki, Microsoft and Member of CIPS Ontario</p>
+          <p className="md:text-xl">
+            Web and Software Developer with expertise in Cybersecurity with
+            Certification from University of Michigan, Google, University of
+            Helsinki, Microsoft and Member of CIPS Ontario
+          </p>
           {/* Buttons */}
           <div className="flex gap-4 lg:w-full">
-            <div className="p-4 rounded-lg ring-1 ring-black bg-black text-white cursor-pointer" onClick={downloadResume}>See my Resume</div>
-            <div className="p-4 rounded-lg ring-1 ring-black bg-black text-white cursor-pointer"><Link href="/portfolio">See my Work</Link></div>
+            <div
+              className="p-4 rounded-lg ring-1 ring-black bg-black text-white cursor-pointer"
+              onClick={downloadResume}
+            >
+              See my Resume
+            </div>
+            <Link
+              href="/portfolio"
+              className="p-4 rounded-lg ring-1 ring-black bg-black text-white cursor-pointer"
+            >
+              Hire Me
+            </Link>
           </div>
         </div>
       </div>
